e4fc6f279c50d8fc59513016b19de7e6110f3ec6
Amy Ahn <amy.ahn.kr@gmail.com>
2024-12-18 19:26:05 -0500

    create pointer tracking with X, Y coordinates

---
+++ src/AppXY.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './AppXY.css';
+
+export default function AppXY() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  return (
+    <div
+      className='container'
+      onPointerMove={(e) => {
+        console.log('clientX:', e.clientX, 'clientY:', e.clientY);
+        setX(e.clientX);
+        setY(e.clientY);
+      }}
+    >
+      <div
+        className='pointer'
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+      />
+    </div>
+  );
+}
