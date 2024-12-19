2ee9ca3beb9f85bef589a38821d6884a33e66f1e
Amy Ahn <amy.ahn.kr@gmail.com>
2024-12-18 19:37:13 -0500

    Refactor state to use object for position updates

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,21 +2,22 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
 
   return (
     <div
       className='container'
       onPointerMove={(e) => {
         console.log('clientX:', e.clientX, 'clientY:', e.clientY);
-        setX(e.clientX);
-        setY(e.clientY);
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // only horizontal movement is allowed, update only Y
+        // setPosition((prev) => ({ x: e.clientX, y: prev.y }));
+        setPosition((prev) => ({ ...prev, x: e.clientX }));
       }}
     >
       <div
         className='pointer'
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       />
     </div>
   );
