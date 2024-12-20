ff3c0f13cb3d3d24b8a58035724997b4fbc7e5b9
Amy Ahn <amy.ahn.kr@gmail.com>
2024-12-19 23:46:44 -0500

    loading and error

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   // State to hold the list of products
   const [products, setProducts] = useState([]);
   // State to manage the checkbox status
@@ -10,6 +12,9 @@ export default function Products() {
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+
     // Fetch product data
     // If the checkbox is checked, fetch 'sale_product.json', otherwise fetch 'product.json'
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
@@ -17,13 +22,18 @@ export default function Products() {
       .then((data) => {
         console.log('data:', data); // Log the fetched data to the console
         setProducts(data); // Update the products state with the fetched data
-      });
+      })
+      .catch((e) => setError('Error!'))
+      .finally(() => setLoading(false));
 
     // Cleanup function runs when the component unmounts or 'checked' changes
     return () => {
       console.log('clean'); // Log 'clean' to indicate cleanup
     };
   }, [checked]); // Dependency: Re-run fetch when 'checked' changes
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <>
